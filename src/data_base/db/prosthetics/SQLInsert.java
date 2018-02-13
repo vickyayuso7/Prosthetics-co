3716fa89e57984dca3e30c8a74a41070382c9a7e
Sloth Thy Lord <Sloth Thy Lord@TocinoSublime>
2018-02-13 17:06:45 +0100

    created a basic insert method. Requires adding control code.

--- src/data_base/db/prosthetics/SQLInsert.java
+++ src/data_base/db/prosthetics/SQLInsert.java
@@ -1,44 +1,290 @@
 package data_base.db.prosthetics;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import  java.sql.*;
+import pojos.db.prosthetics.*;
 
 public class SQLInsert {
-
-	static void Insert(){
+String report;
+Boolean failed;
+String insert;
+//Added a constructor so that everyone is happy about the inserting method being an object.
+//you mysogenist objectifying pieces of trash.
+public void SQLinsert() {
+	this.report="not connected";
+	this.failed=false;
+	this.insert="";
+}
+/*one method that may be called upon whenever needed that does the job of inserting whatever is 
+ * passed on (hopefully some useful objects that have to be PUBLIC in order for this package to 
+ * make use of them and not any random garbage)*/
+	String Insert(Material mat, Client cln, Features ftr, Adress adr, Payment pmnt, Prosthetic prstc )
+	{
+		this.failed=false;
+		this.insert="";
+		this.report="";
 		try {
-			// Open database connection
+			/*BTW the code does a very poor job of checking if all given parameters are valid
+			 * insertions. It should check for nulls where there shouldn't be any and those related 
+			 * things... It could be handled by another method but just for the sake of being 
+			 * f*cking hysterical about mistakes, I will implement it... Later...*/
+			Class.forName("org.sqlite.JDBC");
+			Connection c= DriverManager.getConnection("jdbc:sqlite:./db/prosthetics");
+			report="Connection established ";
+			Statement stm1=c.createStatement();
+			try {
+				this.insert="INSERT INTO Address (id, country, post code, street,town, number) VALUES ("
+						+adr.getId()+","+adr.getCountry() + ", " +adr.getPostCode()+", "+adr.getStreet()+
+						", "+adr.getTown()+", "+adr.getNumber()+ ")";
+				stm1.executeUpdate(insert);
+				report=report+"and inserted Adress:"+adr.getId()+"/n";
+			}
+			catch(Exception ex){
+				report=report+"but failed to insert address ";
+				failed=true;
+			}
+			try {
+				if(failed ==false) {
+					this.insert="INSERT INTO Client (id, name, age, gender, measures, address_id)"
+							+"VALUES ("+cln.getId()+","+ cln.getName()+", "+cln.getAge()+", "
+							+cln.getGender()+", "+cln.getMeasures+", "+cln.getAdddress_id()+")";
+					stm1.executeUpdate(insert);
+					report=report+"inserted Client:"+cln.getName()+"/n";
+				}
+				else {
+					System.out.println(report+"and therefore could not proceed on./n");
+				}
+			}
+			catch(Exception ex) {
+					report=report+"but failed to insert client";
+					failed=true;
+			}
+			try {
+				if(failed==false) {
+					this.insert="INSERT INTO Features (id, style, sensibility,enhanced_movement)"
+							+"VALUES ("+ftr.getId()+", "+ftr.getStyle()+", "+ftr.getSensibility()
+							+", "+ftr.getEnhanced_movement()+")";
+					stm1.executeUpdate(insert);
+					report=report+"inserted Features"+ftr.getId()+"/n";
+				}
+				else {
+					report=report+"and therefore could not proceed on./n";
+				}
+			}
+			catch(Exception ex) {
+				report=report+"but failed to insert features";
+				failed=true;
+			}
+			try {
+				if(failed==false) {
+					this.insert="INSERT INTO Material (id, type, price, provider) VALUES ("
+							+mat.getId()+", "+mat.getType()+", "+mat.getPrice()+", "+mat.getProvider()
+							+")";
+					stm1.executeUpdate(insert);
+					report=report+"inserted material"+mat.getId()+"/n";
+				}
+				else {
+					report=report+"and therefore could not proceed on,/n";
+				}
+			}
+			catch(Exception ex) {
+				report =report+"but failed to insert Materials";
+				failed=true;
+			}
+			try {
+				if(failed==false) {
+					this.insert="INSERT INTO Payments (id, deadline, isbn, method) VALUES("
+							+pmnt.getId()+", "+pmnt.getDeadline()+", "+pmnt.getIsbn()+", "
+							+pmnt.getMethod()+")";
+					stm1.executeUpdate(insert);
+					report=report+"inserted payments"+pmnt.getId()+"/n";
+				}
+				else {
+					report=report+"and therefore could not proceed on,/n";
+				}
+			}
+			catch(Exception ex) {
+				report=report+"but failed to insert Payments";
+				failed=true;
+			}
+			try {
+				if(failed==false) {
+					this.insert="INSERT INTO Prosthetics (id, size, weight, type_of_functionality"
+							+", color, type_of_amputation, payment_id) VALUES("+prstc.getId()+", "
+							+prstc.getSize()+", "+prstc.getWeight()+", "+prstc.getType_of_functionality()
+							+", "+prstc.getColor()+", "+prstc.getType_of_amputation()+", "+prstc.getPayment_id
+							+")";
+					stm1.executeUpdate(insert);
+					report=report+"inserted prosthetics"+prstc.getId()+"/n";
+				}
+				else {
+					report=report +"and therefore could not proceed on /n";
+					failed=true;
+				}
+			}
+			catch(Exception ex) {
+				report=report+"but failed to insert prosthetics";
+				failed=true;
+			}
+			c.close();
+		}
+		catch(Exception sql) {
+			report="failed to conect to database";
+		}
+		return(report);
+	}
+	/*If you are reading this you probably skimmed through the code wondering WTF was going on
+	 * let me enlight you,this is taking all the possible entities and inserting them in one go.
+	 * supposedly used when a new client is created with all it's data being introduced at once
+	 * I will use Java's overload policy to create a few variants of the same command that will
+	 * allow users to insert certain tables without having to give in the whole badge */
+	//the last issue was fixed. 
+	String Insert(Material mat) {
+		this.report="";
+		this.insert="";
+		try {
 			Class.forName("org.sqlite.JDBC");
 			Connection c = DriverManager.getConnection("jdbc:sqlite:./db/prosthetics");
-			c.createStatement().execute("PRAGMA foreign_keys=ON");
-			System.out.println("Database connection opened.");
-
-			// Get the user info from the command prompt
-			System.out.println("Please, input the payment info:");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("Deadline: ");
-			String name = reader.readLine();
-			System.out.print("Address: ");
-			String address = reader.readLine();
-
-			// Insert new record: begin
-			Statement stmt = c.createStatement();
-			String sql = "INSERT INTO departments (name, address) "
-					+ "VALUES ('" + name + "', '" + address	+ "');";
-			stmt.executeUpdate(sql);
-			stmt.close();
-			System.out.println("Department info processed");
-			System.out.println("Records inserted.");
-			// Insert new record: end
-
-			// Close database connection
+			Statement stm1= c.createStatement();
+			try {
+					this.insert="INSERT INTO Material (id, type, price, provider) VALUES ("
+							+mat.getId()+", "+mat.getType()+", "+mat.getPrice()+", "+mat.getProvider()
+							+")";
+					stm1.executeUpdate(insert);
+					report=report+"inserted material"+mat.getId()+"/n";
+			}
+			catch(Exception ex) {
+				report ="failed to insert Materials";
+			}
 			c.close();
-			System.out.println("Database connection closed.");
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
+		catch(Exception ex) {
+			report="failed to connect to database";
+		}
+		return(this.report);
 	}
+	String Insert(Features ftr) {
+		this.report="";
+		this.report="";
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection c=DriverManager.getConnection("jdbc.sqlite:./db/prosthetics");
+			Statement stm1=c.createStatement();
+			try {
+				this.insert="INSERT INTO Features (id, style, sensibility,enhanced_movement)"
+						+"VALUES ("+ftr.getId()+", "+ftr.getStyle()+", "+ftr.getSensibility()
+						+", "+ftr.getEnhanced_movement()+")";
+				stm1.executeUpdate(insert);
+				report="inserted Features"+ftr.getId()+"/n";
+				c.close();
+			}
+			catch(Exception ex) {
+				report="failed to insert features";
+			}
+		}
+		catch(Exception ex) {
+			report="failed to connect to database";
+		}
+		return (report);
+	}
+	String Insert(Prosthetic prstc) {
+		this.report="";
+		this.report="";
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection c=DriverManager.getConnection("jdbc.sqlite:./db/prosthetics");
+			Statement stm1=c.createStatement();
+			try {
+				this.insert="INSERT INTO Prosthetics (id, size, weight, type_of_functionality"
+						+", color, type_of_amputation, payment_id) VALUES("+prstc.getId()+", "
+						+prstc.getSize()+", "+prstc.getWeight()+", "+prstc.getType_of_functionality()
+						+", "+prstc.getColor()+", "+prstc.getType_of_amputation()+", "+prstc.getPayment_id
+						+")";
+				stm1.executeUpdate(insert);
+				report="inserted prosthetics"+prstc.getId()+"/n";
+			}
+			catch(Exception ex) {
+				report="failed to insert prosthetics";
+			}
+			c.close();
+		}
+		catch(Exception ex) {
+			report="failed to connect to database";
+		}
+		return(report);
+	}
+	String Insert(Address adr) {
+		this.report="";
+		this.report="";
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection c=DriverManager.getConnection("jdbc.sqlite:./db/prosthetics");
+			Statement stm1=c.createStatement();
+			try {
+				this.insert="INSERT INTO Address (id, country, post code, street,town, number) VALUES ("
+						+adr.getId()+","+adr.getCountry() + ", " +adr.getPostCode()+", "+adr.getStreet()+
+						", "+adr.getTown()+", "+adr.getNumber()+ ")";
+				stm1.executeUpdate(insert);
+				report="inserted Adress:"+adr.getId()+"/n";
+			}
+			catch(Exception ex){
+				report="failed to insert address ";
+			}
+			c.close();
+		}
+		catch(Exception ex) {
+			report="failed to connect to database";
+		}
+		return(report);
+	}
+	String Insert(Client cln) {
+		this.report="";
+		this.insert="";
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection c = DriverManager.getConnection("jdbc:sqlite:./db/prosthetics");
+			Statement stm1= c.createStatement();
+			try {
+				this.insert="INSERT INTO Client (id, name, age, gender, measures, address_id)"
+						+"VALUES ("+cln.getId()+","+ cln.getName()+", "+cln.getAge()+", "
+						+cln.getGender()+", "+cln.getMeasures+", "+cln.getAdddress_id()+")";
+				stm1.executeUpdate(insert);
+				report=report+"inserted Client:"+cln.getName()+"/n";
+			}
+			catch(Exception ex) {
+					report="failed to insert client";
+			}
+			c.close();
+		}
+		catch(Exception ex) {
+			report="failed to connect to database";
+		}
+		return(report);
+	}
+	String Insert(Payment pmnt) {
+		this.report="";
+		this.insert="";
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection c = DriverManager.getConnection("jdbc:sqlite:./db/prosthetics");
+			Statement stm1= c.createStatement();
+			try {
+				this.insert="INSERT INTO Payments (id, deadline, isbn, method) VALUES("
+						+pmnt.getId()+", "+pmnt.getDeadline()+", "+pmnt.getIsbn()+", "
+						+pmnt.getMethod()+")";
+				stm1.executeUpdate(insert);
+				report=report+"inserted payments"+pmnt.getId()+"/n";
+			}
+			catch(Exception ex){
+				report="failed to insert address ";
+			}
+			c.close();
+	}
+		catch(Exception ex) {
+			report="failed to connect to database";
+		}
+		return(report);
+	}
+/*methods added to enable the creation of only one object instead of having to give in a complete
+ * set of data.*/
+
 }
