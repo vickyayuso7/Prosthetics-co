bbfb7266dbf837ef57b934408d53f06994fba8c1
Sloth Thy Lord <Sloth Thy Lord@TocinoSublime>
2018-02-15 11:13:01 +0100

    material

--- src/pojos/db/prosthetics/Material.java
+++ src/pojos/db/prosthetics/Material.java
@@ -1,77 +1,50 @@
 package pojos.db.prosthetics;
-import java.sql.*;
 
-class Material{
-	static String Insert(float price, String type, String provider) {
-		String report;
-		boolean connected=false;
-		try {
-			Class.forName("org.sqlite.JDBC");
-			Connection connect= DriverManager.getConnection("jdbc:sqlite:./db/prosthetics");
-			report="Connection extablished ";
-			connected=true;
-			Statement stm1= connect.createStatement();
-			String insert="INSERT INTO Materials (Price, Type, Provider) VALUES ("+price
-					+", "+type+", "+provider;
-			try {
-				stm1.executeUpdate(insert);
-				report="and managed to insert new data into tables";
-			}
-			catch(Exception sql) {
-				report="but failed to insert new data into tables"
-						+ "(Exception thrown from Material.java line 15)";
-			}
-			stm1.close();
-		}
-		catch(Exception sql){
-			if(connected==false) {
-				report="failed to connect to database (Exception thrown from Material.Java line 8)";
-			}
-			else {
-				report="unknown Exception thrown from Materials.java,connection established but "
-						+ "failed to accomplish task.";
-			}
-			
-		}
-		return(report);
+import java.io.Serializable;
+
+public class Material implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5860436926701456573L;
+	float price;
+	String type;
+	String provider;
+	int id;
+	Material(int id, float prc, String tp, String prv){
+		this.price=prc;
+		this.type=tp;
+		this.provider=prv;
+		this.id=id;
 	}
-	/*supports all text format inputs.*/
-	static String Insert(String price, String type, String provider) {
-		String report;
-		boolean connected=false;
-		try {
-			Class.forName("org.sqlite.JDBC");
-			Connection connect= DriverManager.getConnection("jdbc:sqlite:./db/prosthetics");
-			report="Connection extablished ";
-			connected=true;
-			Statement stm1= connect.createStatement();
-			String insert="INSERT INTO Materials (Price, Type, Provider) VALUES ("+price
-					+", "+type+", "+provider;
-			try {
-				stm1.executeUpdate(insert);
-				report="and managed to insert new data into tables";
-			}
-			catch(Exception sql) {
-				report="but failed to insert new data into tables"
-						+ "(Exception thrown from Material.java line 15)";
-			}
-			stm1.close();
-		}
-		catch(Exception sql){
-			if(connected==false) {
-				report="failed to connect to database (Exception thrown from Material.Java line 8)";
-			}
-			else {
-				report="unknown Exception thrown from Materials.java,connection established but "
-						+ "failed to accomplish task.";
-			}
-			
-		}
-		return(report);
+	Material(){
+		this.price=0;
+		this.provider="";
+		this.type="";
+		this.id=0;
 	}
-	static String delete() {
-		/*deletion code goes here*/
-		String report="not implemented yet";
-		return (report);
+	public void setPrice(float price){
+		this.price=price;
+	}
+	public void setType(String type) {
+		this.type=type;
+	}
+	public void setProvider(String provider) {
+		this.provider=provider;
+	}
+	public void setId(int id) {
+		this.id=id;
+	}
+	public float getPrice() {
+		return(this.price);
+	}
+	public String getType() {
+		return(this.type);
+	}
+	public String getProvider() {
+		return(this.provider);
+	}
+	public int getId() {
+		return(this.id);
 	}
 }
