a35208ddf65947ad69b857979bdd483a101cab1f
vicky <vayuso97@gmail.com>
2018-02-15 17:06:44 +0100

    material

--- src/pojos/db/prosthetics/Material.java
+++ src/pojos/db/prosthetics/Material.java
@@ -2,6 +2,8 @@
 package pojos.db.prosthetics;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Material implements Serializable{
 	/**
@@ -12,18 +14,48 @@ public class Material implements Serializable{
 	String type;
 	String provider;
 	int id;
+	private List<Prosthetics> prosthetics;
 	
-	Material(int id, float prc, String tp, String prv){
+	Material(int id, float prc, String tp, String prv, List<Prosthetics> prosthetics){
 		this.price=prc;
 		this.type=tp;
 		this.provider=prv;
 		this.id=id;
+		this.prosthetics = new ArrayList<Prosthetics>();
 	}
 	Material(){
 		this.price=0;
 		this.provider="";
 		this.type="";
 		this.id=0;
+		this.prosthetics = new ArrayList<Prosthetics>();
+	}
+	
+	@Override
+	public String toString() {
+		return "Client [id=" +id+ "price=" + price + ", type=" + type + ", provider=" + provider +
+				"]";
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Material other = (Material) obj;
+		if (id != other.id)
+			return false;
+		return true;
 	}
 	public void setPrice(float price){
 		this.price=price;
@@ -48,5 +80,27 @@ public class Material implements Serializable{
 	}
 	public int getId() {
 		return(this.id);
+	}
+	
+	public List<Prosthetics> getProsthetics() {
+		return prosthetics;
+	}
+
+	public void setProsthetics(List<Prosthetics> prosthetics) {
+		this.prosthetics = prosthetics;
+	}
+
+	
+	public void addProsthetics(Prosthetics prosthetic) {
+		if (!prosthetics.contains(prosthetic)) {
+			this.prosthetics.add(prosthetic);
+		}
+	}
+
+	// Additional method to remove from a list
+	public void removeProsthetics(Prosthetics prosthetic) {
+		if (prosthetics.contains(prosthetic)) {
+			this.prosthetics.remove(prosthetic);
+		}
 	}
 }
