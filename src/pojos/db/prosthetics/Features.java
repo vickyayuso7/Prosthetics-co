cd18bffb05d82e229a153c43ed569f87f1d278c4
Sloth Thy Lord <Sloth Thy Lord@TocinoSublime>
2018-02-15 11:06:56 +0100

    check for prosthetics

--- src/pojos/db/prosthetics/Features.java
+++ src/pojos/db/prosthetics/Features.java
@@ -1,43 +1,109 @@
 package pojos.db.prosthetics;
-import java.sql.*;
 
-class Features {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Features implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3282893934193593462L;
+	private int id;
+	private String style;
+	boolean sensibility;
+	boolean enhanced_movement;
+	private List<Features> features;	
 	
-	static String insert(String style, String	shape, boolean sensibility) {
-		String report;
-		boolean inserted;
-		try {
-			Class.forName("org.sqlite.JDBC");
-			Connection connect = DriverManager.getConnection("jdbc:sqlite:./db/prosthetics");
-			report= "connection established ";
-			Statement st1= connect.createStatement();
-			String insert= "INSERT INTO Features {(Style, Shape, Sensibility) VALUES ("
-					+style+", "+shape+", "+sensibility;
-			try {
-			st1.executeUpdate(insert);
-			inserted=true;
-			}
-			catch (Exception ex){
-				report=report+"but failed to insert new data.";
-				inserted=false;
-			}
-			st1.close();
-			if(inserted==true) {
-				report= report+"and managed to insert new data:"+style +shape +sensibility;
-			}
-			return(report);
-		}
-		catch(Exception Io) {
-			report="unable to establish a connection with repository";
-			return(report);
-		}
-		
-	}
-	static String delete(String style, String shape, boolean sensibility ) {
-		String report;
-		/*Deletion code for a specific combination of atributes*/
-		report="not implemented yet.";
-		return(report);
+	Features(int id, String style, boolean sensibility, boolean enhanced_movement){
+		this.id=id;
+		this.sensibility=sensibility;
+		this.enhanced_movement=enhanced_movement;
+		this.style=style;
+		this.features= new ArrayList<Features>();
 	}
 	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Features other = (Features) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	
+	public String toString() {
+		return "Features [id=" +id+ "sensibility="+sensibility+"enhanced_movement="+enhanced_movement+"style="+style;
+	}
+	
+	
+
+	public String getStyle() {
+		return style;
+	}
+
+
+	public void setStyle(String style) {
+		this.style = style;
+	}
+
+
+	public List<Features> getFeatures() {
+		return features;
+	}
+
+
+	public void setFeatures(List<Features> features) {
+		this.features = features;
+	}
+	
+	public void addFeatures(Features feature) {
+		if(!features.contains(feature)) {
+			this.features.add(feature);
+		}
+	}
+	
+	public void removeFeatures(Features feature) {
+		if(features.contains(feature)) {
+			this.features.remove(feature);
+		}
+	}
+	public boolean getSensibility() {
+		return sensibility;
+	}
+	public boolean getEnhanced_movement() {
+		return enhanced_movement;
+	}
+	public void setType(String st) {
+		this.style=st;
+	}
+	public void setId(int id) {
+		this.id=id;
+	} 
+	public void setSensibility(boolean sens) {
+		this.sensibility=sens;
+	}
+	public void setEnhanced_movement(boolean movm) {
+		this.enhanced_movement=movm;
+	}
+	public int getId() {
+		return(this.id);
+	}
 }
