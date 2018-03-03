dfb9d2039eafe9ed780b8f314e6eed385ce87d32
Sloth Thy Lord <Sloth Thy Lord@TocinoSublime>
2018-03-03 20:48:55 +0100

    actually made into a usable class from the original main

--- src/data_base/db/prosthetics/SQLConnect.java
+++ src/data_base/db/prosthetics/SQLConnect.java
@@ -4,17 +4,27 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class SQLConnect {
+	Connection connect;
 
-	public static void main(String args[]) {
+	public void establishConnection() {
 		try {
 			Class.forName("org.sqlite.JDBC");
-			Connection c = DriverManager.getConnection("jdbc:sqlite:./db/company.db");
-			c.createStatement().execute("PRAGMA foreign_keys=ON");
+			connect = DriverManager.getConnection("jdbc:sqlite:./db/company.db");
+			connect.createStatement().execute("PRAGMA foreign_keys=ON");
 			System.out.println("Database connection opened.");
-			c.close();
-			System.out.println("Database connection closed.");
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("could not establish a connection.");
+		}
+	}
+	public Connection getConnection() {
+		return(this.connect);
+	}
+	public void closeConnection() {
+		try {
+			this.connect.close();
+		}
+		catch(Exception e) {
+			System.out.println("could not close the database.");
 		}
 		
 	}
