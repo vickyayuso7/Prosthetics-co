a1c48156e462671db1ba73ab7a229ca09d54706c
vicky <vayuso97@gmail.com>
2018-03-16 15:41:40 +0100

    fghjkl

--- src/graphicInterface/NewMaterial.java
+++ src/graphicInterface/NewMaterial.java
@@ -1,47 +1,4 @@
-<<<<<<< HEAD
-package graphicInterface;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-public class NewMaterial extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					NewMaterial frame = new NewMaterial();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public NewMaterial() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
-
-}
-=======
 package graphicInterface;
 
 import java.awt.BorderLayout;
@@ -155,4 +112,3 @@ public class NewMaterial extends JFrame {
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/vickyayuso7/Prosthetics-co.git
