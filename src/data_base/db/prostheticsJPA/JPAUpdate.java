9a714472bbc6727906e0d436ca25852b04f3587d
vicky <vayuso97@gmail.com>
2018-05-03 17:31:33 +0200

    JPAUpdate fixed.

--- src/data_base/db/prostheticsJPA/JPAUpdate.java
+++ src/data_base/db/prostheticsJPA/JPAUpdate.java
@@ -6,7 +6,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import pojos.db.prosthetics.User;
 
@@ -15,42 +14,35 @@ public class JPAUpdate {
 
 private static EntityManager connection;
 	
-	private static void printUsers() throws NumberFormatException, IOException {
+	private static void printUsers() throws Exception {
 		Query q1 = connection.createNativeQuery("SELECT * FROM users", User.class);
 		List<User> users = (List<User>) q1.getResultList();
 		for (User user : users) {
 			System.out.println(user);
-		}
-	}
-
-	
-		
-		//JPAConnect connection= new JPAConnect();
-		//connection.establishConnection();
-		
+		}		
 		/*connection = Persistence.createEntityManagerFactory("user").createEntityManager();
 		connection.getTransaction().begin();
 		connection.createNativeQuery("PRAGMA foreign_keys=ON").executeUpdate();
-		connection.getTransaction().commit();*/
-
+		connection.getTransaction().commit();
+*/
 		
 		System.out.println("Users' names:");
 		printUsers();
 		System.out.print("Type a name to change its password:");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		int user_id = Integer.parseInt(reader.readLine());
-		Query q2 = JPAConnect.getEntityManager().createNativeQuery("SELECT * FROM users WHERE id = ?", User.class);
-		q1.setParameter(1, user_id);
+		Query q2 = connection.createNativeQuery("SELECT * FROM users WHERE id = ?", User.class);
+		q2.setParameter(1, user_id);
 		User user = (User) q1.getSingleResult();
 		System.out.print("Type a new password:");
 		String newPassword = reader.readLine();
 		
-		JPAConnect.getEntityManager().getTransaction().begin();
+		connection.getTransaction().begin();
 		user.setPassword(newPassword);
-		JPAConnect.getEntityManager().getTransaction().commit();
+		connection.getTransaction().commit();
 		
 	
-		JPAConnect.getEntityManager().close();
+		connection.close();
 	}
 }
 
