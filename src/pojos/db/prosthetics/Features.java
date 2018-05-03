62b5989c819d692b1bebf9bbcc9c998433a76031
martinasg <marta010796@hotmail.com>
2018-05-03 18:21:57 +0200

    xml

--- src/pojos/db/prosthetics/Features.java
+++ src/pojos/db/prosthetics/Features.java
@@ -17,15 +17,17 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.TableGenerator;
+import javax.xml.bind.annotation.*;
+
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
-
-import com.sun.xml.internal.txw2.annotation.XmlElement;
-
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 @Entity
 @Table(name = "features")
 
