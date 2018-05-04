bc8b9921a9ac0ba54d9ee51e7e66be498d7251e7
MARIA JESUS <MARIA JESUS@Pumuky>
2018-05-04 15:57:41 +0200

    JPADelete

--- src/data_base/db/prostheticsJPA/JPADelete.java
+++ src/data_base/db/prostheticsJPA/JPADelete.java
@@ -1,25 +1,53 @@
 package data_base.db.prostheticsJPA;
 
 import java.util.List;
+import java.io.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import pojos.db.prosthetics.User;
 
-
 public class JPADelete {
-	private static EntityManager manager;
+	private static EntityManager connection;
 
+	
+		
 	private static void printUser() {
-		Query q1 = manager.createNativeQuery("SELECT * FROM User", User.class);
+		Query q1 = connection.createNativeQuery("SELECT * FROM User", User.class);
 		List<User> users = (List<User>) q1.getResultList();
 		for (User tempUser : users ) {
 			System.out.println(tempUser);
 		}
+	
+
+	System.out.println("List of users:");
+	printUser();
+	System.out.print("Choose a user you want to delete:");
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	
+	int user_id=0;
+	try {
+		user_id = Integer.parseInt(reader.readLine());
+	} catch (NumberFormatException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
-	public static void main(String[] args){
-		printUser();
-		
+	Query q2 = connection.createNativeQuery("SELECT * FROM users WHERE id = ?", User.class);
+	q2.setParameter(1, user_id);
+	User userDeleted = (User) q2.getSingleResult();
+
+	// Begin transaction
+	connection.getTransaction().begin();
+	// Store the object
+	connection.remove(userDeleted);
+	// End transaction
+	connection.getTransaction().commit();
+
+	// Close the entity manager
+	connection.close();
 	}
 }
