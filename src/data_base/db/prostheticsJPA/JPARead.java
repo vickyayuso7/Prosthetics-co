e44bbe2ebf21b8ceb794f815047271106ad83a30
vicky <vayuso97@gmail.com>
2018-05-04 16:25:25 +0200

    complete jpa

--- src/data_base/db/prostheticsJPA/JPARead.java
+++ src/data_base/db/prostheticsJPA/JPARead.java
@@ -9,61 +9,53 @@ import javax.persistence.Query;
 
 import pojos.db.prosthetics.User;
 
-
-
 public class JPARead {
 
 	public static EntityManager connection;
-	
-	private static void readUser(){
-	
-	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	
-	System.out.print("Write the department's name: " );
-	String name = null;
-	try {
-		name = reader.readLine();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	System.out.println("Matching departments:");
-	Query q1= connection.createNamedQuery("SELECT * FROM users WHERE id = ?", User.class);
-	
-	
-	
 
-	
-	q1.setParameter(1, "%" + name + "%");
-	List<User> user = (List<User>) q1.getResultList(); //a list of the objects we put here
-	// Print the departments
-	for (User user1 : user) {
-		System.out.println(user1);
-	}
-	
-	
-	
-	System.out.print("Write the user's ID: ");
-	int user_id;
-	try {
-		user_id = Integer.parseInt(reader.readLine());
-	} catch (NumberFormatException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	Query q2 = connection.createNativeQuery("SELECT * FROM users WHERE id = ?", User.class);//we pass the type of object we are gonna retrieve
-	q2.setParameter(1, name);// fill the question mark
-	User paco = (User) q2.getSingleResult();//only call if we are totally sure we are getting a single result
-	// Print the department
-	System.out.println(paco);
-	
-	// Close the entity manager
-	connection.close();
-		
-	
-}
-}
+	private static void readUser() {
 
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("Write the department's name: ");
+		String name = null;
+		try {
+			name = reader.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Matching departments:");
+		Query q1 = connection.createNamedQuery("SELECT * FROM users WHERE id = ?", User.class);
+
+		q1.setParameter(1, "%" + name + "%");
+		List<User> user = (List<User>) q1.getResultList(); // a list of the objects we put here
+		// Print the departments
+		for (User user1 : user) {
+			System.out.println(user1);
+		}
+
+		System.out.print("Write the user's ID: ");
+		int user_id;
+		try {
+			user_id = Integer.parseInt(reader.readLine());
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Query q2 = connection.createNativeQuery("SELECT * FROM users WHERE id = ?", User.class);// we pass the type of
+																								// object we are gonna
+																								// retrieve
+		q2.setParameter(1, name);// fill the question mark
+		User paco = (User) q2.getSingleResult();// only call if we are totally sure we are getting a single result
+		// Print the department
+		System.out.println(paco);
+
+		// Close the entity manager
+		connection.close();
+
+	}
+}
